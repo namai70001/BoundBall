11b268e0291d8925c8a3dcc1da9c797563b0fc3a
namai.ryuuki <namai.ryuuki@gmail.com>
2019-07-01 19:51:28 +0900

    SceneSingletonクラスを追加

---
+++ Assets/Scripts/Framework/Singleton/SceneSingleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Framework.Singleton
+{
+    public class SceneSingleton<T> : MonoBehaviour where T : new()
+    {
+        private static T instance;
+        public T Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new T();
+
+                return instance;
+            }
+        }
+    }
+}
