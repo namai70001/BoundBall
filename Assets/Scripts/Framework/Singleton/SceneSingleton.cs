f7045f891f4d488f358d5242352a851b2b97cda4
namai.ryuuki <namai.ryuuki@gmail.com>
2019-07-02 12:18:34 +0900

    シングルトンオブジェクトの名前をクラス名のみに修正

--- Assets/Scripts/Framework/Singleton/SceneSingleton.cs
+++ Assets/Scripts/Framework/Singleton/SceneSingleton.cs
@@ -5,13 +5,13 @@ namespace Framework.Singleton
     public class SceneSingleton<T> : MonoBehaviour where T : MonoBehaviour, new()
     {
         private static T instance;
-        public T Instance
+        public static T Instance
         {
             get
             {
                 if (instance == null)
                 {
-                    GameObject obj = new GameObject(typeof(T).ToString());
+                    GameObject obj = new GameObject(typeof(T).Name);
                     instance = obj.AddComponent<T>();
                 }                    
 
