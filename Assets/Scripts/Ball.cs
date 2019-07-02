1fa6beee3a79d3aaa5ea4d5bf1b9594f6c0c0a78
namai.ryuuki <namai.ryuuki@gmail.com>
2019-07-02 13:18:42 +0900

    ボールの処理を追加

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float gravity;
+
+    [SerializeField]
+    private float boundPower;
+
+    private Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move -= new Vector3(0, gravity * Time.deltaTime);
+
+        transform.position += move * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float lineAngle = collision.gameObject.transform.localRotation.z + 90;
+        Vector2 boundDirection = new Vector2(Mathf.Cos(lineAngle),Mathf.Sin(lineAngle));
+
+        move = boundDirection * boundPower;
+    }
+}
