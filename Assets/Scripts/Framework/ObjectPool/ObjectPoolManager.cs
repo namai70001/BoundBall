5aea1f85b01cb3dbe58f6642ff52a879f8c832f2
namai.ryuuki <namai.ryuuki@gmail.com>
2019-07-02 12:24:18 +0900

    オブジェクトプールマネージャーを追加した

---
+++ Assets/Scripts/Framework/ObjectPool/ObjectPoolManager.cs
@@ -0,0 +1,62 @@
+﻿using Framework.Singleton;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework.ObjectPool
+{
+    public class ObjectPoolManager : SceneSingleton<ObjectPoolManager>
+    {
+        private Dictionary<string,List<GameObject>> poolObjDictionary = new Dictionary<string, List<GameObject>>();
+
+        public void CreatePool(string path, int maxCount = 1)
+        {
+            List<GameObject> poolObjList = new List<GameObject>();
+            poolObjDictionary.Add(path, poolObjList);
+
+            for (int i = 0; i < maxCount; i++)
+            {
+                GameObject newObj = CreateNewObject(path);
+            }
+        }
+
+        public GameObject GetObject(string path ,int count = 1)
+        {
+            if(!poolObjDictionary.ContainsKey(path))
+            {
+                CreatePool(path, count);
+            }
+
+            foreach(GameObject obj in poolObjDictionary[path])
+            {
+                if(obj.activeSelf == false)
+                {
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+
+            GameObject newObj = CreateNewObject(path);
+
+            poolObjDictionary[path].Add(newObj);
+            newObj.SetActive(true);
+
+            return newObj;
+        }
+
+        public void DeleteObject(GameObject gameObject)
+        {
+            gameObject.SetActive(false);
+            gameObject.transform.parent = transform;
+        }
+
+        private GameObject CreateNewObject(string path)
+        {
+            GameObject prefab = Resources.Load<GameObject>(path);
+            GameObject poolObj = Instantiate(prefab,transform);
+            poolObj.name = prefab.name + (poolObjDictionary[path].Count + 1);
+            poolObj.SetActive(false);
+
+            return poolObj;
+        }
+    }
+}
