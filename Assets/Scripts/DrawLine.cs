d4406607c38199e40c38d1cefde160d99f3c8069
DESKTOP-72J000Q\erisa <kusanoerisa.hal@gmail.com>
2019-07-08 18:13:42 +0900

    線を描いたときに画像が表示するのを追加しました

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Framework.ObjectPool;
 
 public class DrawLine : MonoBehaviour
 {
     public GameObject linePrefab;
+    public GameObject hasuPrefab;
     public float lineLength = 0.2f; //線の長さ
     public float lineWidth = 0.1f;  //線の幅
+    Vector3 startPos;
+    Vector3 LineStartPos;
+    Vector3 endPos;
+    int cubecnt;
 
     private Vector3 touchPos;
 
     void Start()
     {
+        //ObjectPoolManager.Instance.CreatePool("cube",1);
 
     }
 
@@ -25,20 +32,32 @@ public class DrawLine : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
+            cubecnt = 0;
             touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             touchPos.z = 0;
+            LineStartPos = touchPos;
         }
 
         if (Input.GetMouseButton(0))
         {
 
-            Vector3 startPos = touchPos;
-            Vector3 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            startPos = touchPos;
+            endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             endPos.z = 0;
+            //ObjectPoolManager.Instance.GetObject("cube");
 
             if ((endPos - startPos).magnitude > lineLength)
             {
-                GameObject obj = Instantiate(linePrefab, transform.position, transform.rotation) as GameObject;
+                //ObjectPoolManager.Instance.GetObject("cube");
+                //GameObject obj = Instantiate(linePrefab, transform.position, transform.rotation) as GameObject;
+                GameObject obj = ObjectPoolManager.Instance.GetObject("cube");
+                /*linePrefab.transform.position = (startPos + endPos) / 2;
+                linePrefab.transform.right = (endPos - startPos).normalized;
+
+                linePrefab.transform.localScale = new Vector3((endPos - startPos).magnitude, lineWidth, lineWidth);
+
+                linePrefab.transform.parent = this.transform;
+                */
                 obj.transform.position = (startPos + endPos) / 2;
                 obj.transform.right = (endPos - startPos).normalized;
 
@@ -47,7 +66,26 @@ public class DrawLine : MonoBehaviour
                 obj.transform.parent = this.transform;
 
                 touchPos = endPos;
+                cubecnt++;
             }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            GameObject hasu = ObjectPoolManager.Instance.GetObject("kari_hasu");
+            //GameObject hasu = Instantiate(hasuPrefab, transform.position, transform.rotation) as GameObject;
+
+            Vector2 Line = new Vector2((endPos.x - LineStartPos.x),(endPos.y - LineStartPos.y));
+
+            hasu.transform.position = new Vector3(LineStartPos.x + 1, LineStartPos.y, 0);
+            hasu.transform.localScale = new Vector3(cubecnt, 1, 10);
+
+            float angle = Mathf.Atan2(Line.y, Line.x);
+
+            angle *= 180;
+            angle /= Mathf.PI;
+            hasu.transform.Rotate(new Vector3(0, 0, angle));
         }
     }
 }
+
