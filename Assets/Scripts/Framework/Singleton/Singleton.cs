c3a6600c52f0ae244477bbe598ffe025e8df7db5
namai.ryuuki <namai.ryuuki@gmail.com>
2019-07-11 07:55:09 +0900

    オブジェクトでないシングルトンクラスを追加

---
+++ Assets/Scripts/Framework/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿﻿using UnityEngine;
+
+namespace Framework.Singleton
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour, new()
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
